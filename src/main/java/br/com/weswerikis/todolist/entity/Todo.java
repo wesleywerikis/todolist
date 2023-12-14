2c00105f9516607d76703dab41714e5490423a4c
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-14 05:30:16 -0300

    Adiocionado anotação @AllArgsConst e NoArgsConst - @NoBlank nos atributos nome e descricao - Criado um construtor Todo com parâmetros

--- src/main/java/br/com/weswerikis/todolist/entity/Todo.java
+++ src/main/java/br/com/weswerikis/todolist/entity/Todo.java
@@ -5,22 +5,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "todos")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
     private boolean realizado;
     private int prioridade;
 
-
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 }
